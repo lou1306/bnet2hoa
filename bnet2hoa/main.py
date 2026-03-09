989346d1f12800e530325276332a62bbd40f79a9
Luca Di Stefano <luca.di.stefano@tuwien.ac.at>
2026-03-09 21:30:17 +0100

    Fix API of get_worker_fn()
    The worker now returns the label of a transition as a list of tuples
    of integers: this encodes a formula in DNF form.

--- bnet2hoa/main.py
+++ bnet2hoa/main.py
@@ -78,6 +78,22 @@ def state_to_int(state: dict, aps: list[str]) -> int:
 
 
 def get_worker_fn(primes: dict, allow_stuttering: bool = False) -> Callable[[int], dict]:  # noqa: E501
+    """Return a function that computes the transition relation for a state.
+
+    The inner function encodes transitions in a DNF, DIMACS-like format.
+    trel[next_state] is a list of tuples of integers. Each tuple is a clause,
+    and the elements of the tuple are the conjuncts of the clause.
+    "i" is a literal referring to the (i+1)th variable.
+    "-i" is a negated literal.
+    "0" is the true constant.
+
+    Args:
+        primes (dict): Prime implicant mapping (As given by BNetToPrime)
+        allow_stuttering (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        Callable[[int], dict]: A worker function.
+    """
     eval_int = get_eval_int_fn(primes)
     all_aps = list(primes.keys())
     ap_index = {ap: i for i, ap in enumerate(all_aps)}
@@ -85,6 +101,9 @@ def get_worker_fn(primes: dict, allow_stuttering: bool = False) -> Callable[[int
     def powerset(iterable):
         s = list(iterable)
         return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+    def clause(true_indexes: tuple[int], all_indexes: tuple[int]) -> str:
+        return tuple(i+1 if i in true_indexes else -(i+1) for i in all_indexes)
 
     def worker(state: int) -> dict:
         trel = {}
@@ -97,21 +116,21 @@ def get_worker_fn(primes: dict, allow_stuttering: bool = False) -> Callable[[int
         diff_indexes = [
             i for i in range(len(all_aps))
             if (differences >> (len(all_aps) - i - 1)) & 1]
-
-        trel[sync_next] = "&".join(f"{i}" for i in diff_indexes)  # noqa: E501
+        # When all bits change, the only possible next state is sync_next
+        trel[sync_next] = [clause(diff_indexes, diff_indexes)]
 
         if state == sync_next:
             # This is an attractor state
-            trel[state] = "t"
+            trel[state] = [(0,)]
             return trel
 
         if same != 0:
             # If no bits are selected from diff_indexes, state does not change
-            trel[state] = "&".join(f"!{i}" for i in diff_indexes)
+            trel[state] = [clause((), diff_indexes)]
         elif not allow_stuttering:
             # If stuttering was not allowed, selecting no update
             # is the same as selecting a synchronous update
-            trel[sync_next] = "&".join(f"!{i}" for i in ap_index.values())
+            trel[sync_next].append(clause((), ap_index.values()))
 
         for indexes in powerset(diff_indexes):
             if len(indexes) == 0 or len(indexes) == len(diff_indexes):
@@ -120,12 +139,11 @@ def get_worker_fn(primes: dict, allow_stuttering: bool = False) -> Callable[[int
             for index in indexes:
                 mask |= (1 << (len(all_aps) - index - 1))
             cur_next = (sync_next & ~mask) | (state & mask)
-
-            guard = "&".join(str(i) if i in indexes else f"!{i}" for i in diff_indexes)  # noqa: E501
+            guard = clause(indexes, diff_indexes)
             if cur_next not in trel:
-                trel[cur_next] = guard
+                trel[cur_next] = [guard]
             else:
-                trel[cur_next] += " | " + guard
+                trel[cur_next].append(guard)
         return trel
     return worker
 
@@ -203,15 +221,24 @@ def main():
             f"[WARNING] Automaton may have up to {len(states):.2e} states.",
             file=sys.stderr)
 
+    def fmt_variable(i: int) -> str:
+        if i == 0:
+            return "t"
+        return f"{i-1}" if i > 0 else f"!{(-i-1)}"
+
     for state in states:
         if state < 0 or state >= num_states:
-            print(f"[WARNING] Skipping invalid state: {state}", file=sys.stderr)
+            print(f"[WARNING] Skipping invalid state: {state}", file=sys.stderr)  # noqa: E501
             continue
         tr = worker(state)
         print(f'State: {state} "{int_to_bin(state, aps)}"')
-        for next_state, guard in tr.items():
+        for next_state, guard_dnf in tr.items():
+            guard = "|".join(
+                "&".join(fmt_variable(i) for i in guard_clause)
+                for guard_clause in guard_dnf)
             print(f'[{guard}] {next_state}')
     print("--END--")
+
 
 if __name__ == "__main__":
     main()
